f19d7b84b8777f5ff61a09b87723add31aba1b56
Galaktro <galaktro@aliensbringchange.com>
2022-10-07 09:51:45 +0200

    auto commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# Serve
+
+Simple webserver that hosts html, css and binary files on your localhost. Can be used to create a pip package that runs from the macOS terminal.
+Does not inject code into your pages and cannot auto refresh. After changing your website code, the browser needs a manual reload.
+
+# Create pip package
+
+Go to the folde that contains pyproject.toml and execute:
+
+```pip install .```
+
+# Usage
+
+Go to a folder in the Terminal that contains index.html and execute:
+
+```serve```
---
+++ src/serve/main.py
@@ -0,0 +1,44 @@
+from http.server import HTTPServer
+from http.server import BaseHTTPRequestHandler
+import os
+import subprocess
+
+port = 8008
+address = "localhost"
+website_directory = os.path.abspath(os.getcwd())
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        path = website_directory + self.path
+        if path.endswith("/"):
+            path += "index.html"
+
+        try:
+            with open(path, "r", encoding="utf-8") as file:
+                text = file.read()
+                self.end_headers()
+                self.wfile.write(bytes(text, "utf-8"))
+
+        except UnicodeDecodeError:
+            self.end_headers()
+            with open(path, "rb") as file:
+                self.wfile.write(file.read())
+        except FileNotFoundError:
+            if path.endswith("index.html"):
+                print("Cannot serve this folder because it does not contain index.html!")
+                exit()
+            print(f"{path} not found")
+
+def start_server():
+    httpd = HTTPServer((address, port), Handler)
+    print(f"--=*> Starting server at {address}:{port} <*=--")
+    print(f"Opening index.html from local directory: {website_directory}.")
+    subprocess.Popen(["open", "-a", "Brave Browser", f"http://127.0.0.1:{port}"])
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("\nServer stopped")
+
+if __name__ == '__main__':
+    start_server()
